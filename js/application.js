b6f449807d69e639dbd7dfeceea8c4028485bbb9
Max Kramer <mckramer@gmail.com>
2015-12-23 11:49:11 -0500

    Add MailChimp form

--- js/application.js
+++ js/application.js
@@ -1,24 +1,3 @@
-//animation to make blockquote appear on scroll
-$(document).ready(function() {
+$(document).ready(function () {
    
-  //hide boxex 
-  $(".bio").hide();
-
-  //init scrolling event heandler
-  $(document).scroll(function(){
-   
-    var docScroll = $(document).scrollTop(), 
-        boxCntOfset = $(".blockquote").offset().top - 100;
-    
- 
-    //when rich top of boxex than fire
-    if(docScroll >= boxCntOfset ) {
-
-      $("#first").fadeIn(200)
-    
-    } else {
-     $("#first").fadeOut(200)
-    
-    }
-  })   
 });
